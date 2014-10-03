90cc41b762fe65aec48efc94046a1f02211a78c5
Mattias Nilsen <kerp@live.se>
2014-10-03 12:22:01 +0200

    Create IComposite interface.

---
+++ app/src/main/java/se/chalmers/katla/model/IComposite.java
@@ -0,0 +1,13 @@
+package se.chalmers.katla.model;
+
+/**
+ * Created by Mattias on 2014-10-03.
+ */
+public interface IComposite {
+
+    public String getText();
+    public String getBaseText();
+    public String setBaseText();
+
+
+}
