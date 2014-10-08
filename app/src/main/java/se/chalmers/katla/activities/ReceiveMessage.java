1aa9c299df5b41aa01b39b34e75106ce0733db92
Anton Freudenthaler <Anton.p.v.Freudenthaler@gmail.com>
2014-10-08 14:43:06 +0200

    Implement AsyncTask in ReceiveMessage

--- app/src/main/java/se/chalmers/katla/activities/ReceiveMessage.java
+++ app/src/main/java/se/chalmers/katla/activities/ReceiveMessage.java
@@ -2,11 +2,22 @@ package se.chalmers.katla.activities;
 
 import android.app.Activity;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.swedspot.automotiveapi.AutomotiveSignal;
+import android.swedspot.automotiveapi.AutomotiveSignalId;
+import android.swedspot.scs.data.SCSFloat;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.swedspot.automotiveapi.AutomotiveFactory;
+import com.swedspot.automotiveapi.AutomotiveListener;
+import com.swedspot.vil.distraction.DriverDistractionLevel;
+import com.swedspot.vil.distraction.DriverDistractionListener;
+import com.swedspot.vil.policy.AutomotiveCertificate;
 
 import se.chalmers.katla.R;
 import se.chalmers.katla.model.IKatla;
@@ -19,6 +30,36 @@ public class ReceiveMessage extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
         model = Katla.getInstance();
+
+        new AsyncTask() {      //Network operation must be run on separate thread than main thread
+            @Override
+            protected Object doInBackground(Object... objects) {
+                AutomotiveFactory.createAutomotiveManagerInstance(
+                        new AutomotiveCertificate(new byte[0]), //Provided certificate
+                        new AutomotiveListener() {
+                            @Override
+                            public void receive(AutomotiveSignal automotiveSignal) {
+                                Log.v("Demo", "Speed is: " + ((SCSFloat) automotiveSignal.getData()).getFloatValue());
+                            }
+
+                            @Override
+                            public void timeout(int i) {
+                            }
+
+                            @Override
+                            public void notAllowed(int i) {
+                            }
+                        },
+                        new DriverDistractionListener() {       //Listener for driver distraction level
+                            @Override
+                            public void levelChanged(DriverDistractionLevel driverDistractionLevel) {
+                                Log.v("Demo", "Driver distraction level is: " + driverDistractionLevel.getLevel());
+                            }
+                        }
+                ).register(AutomotiveSignalId.FMS_WHEEL_BASED_SPEED);
+                return null;
+            }
+        }.execute();
     }
 
 
