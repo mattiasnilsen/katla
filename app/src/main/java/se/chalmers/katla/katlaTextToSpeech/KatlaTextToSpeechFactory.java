5705c0d9772ffc6c7448973af34ab6ca91063c2c
Joel Gustafsson <kalioragus@gmail.com>
2014-10-10 00:13:08 +0200

    Made factory methods static

--- app/src/main/java/se/chalmers/katla/katlaTextToSpeech/KatlaTextToSpeechFactory.java
+++ app/src/main/java/se/chalmers/katla/katlaTextToSpeech/KatlaTextToSpeechFactory.java
@@ -6,11 +6,11 @@ import android.content.Context;
  * Created by Joel on 09/10/2014.
  */
 public class KatlaTextToSpeechFactory {
-    public IKatlaTextToSpeech createKatlaTextToSpeech(Context context) {
+    public static IKatlaTextToSpeech createKatlaTextToSpeech(Context context) {
         return new KatlaTextToSpeech(context);
     }
 
-    public IKatlaTextToSpeech createKatlaTextToSpeech(Context context, String engine) {
+    public static IKatlaTextToSpeech createKatlaTextToSpeech(Context context, String engine) {
         return new KatlaTextToSpeech(context, engine);
     }
 }
