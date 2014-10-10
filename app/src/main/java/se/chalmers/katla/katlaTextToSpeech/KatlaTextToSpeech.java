aadd74516e2248e4b971b65b6c7a597e838cd965
Joel Gustafsson <kalioragus@gmail.com>
2014-10-10 12:14:48 +0200

    Added documentation for Text To Speech service

--- app/src/main/java/se/chalmers/katla/katlaTextToSpeech/KatlaTextToSpeech.java
+++ app/src/main/java/se/chalmers/katla/katlaTextToSpeech/KatlaTextToSpeech.java
@@ -16,6 +16,8 @@ public class KatlaTextToSpeech implements TextToSpeech.OnInitListener, IKatlaTex
     private boolean isReadyToUse;
     private TextToSpeech ttsInstance;
 
+
+
     public KatlaTextToSpeech(Context context) {
         ttsInstance = new TextToSpeech(context, this);
         isReadyToUse = false;
@@ -26,71 +28,102 @@ public class KatlaTextToSpeech implements TextToSpeech.OnInitListener, IKatlaTex
         isReadyToUse = false;
     }
 
+
     @Override
     public void onInit(int i) {
         isReadyToUse = true;
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getDefaultEngine() {
         return ttsInstance.getDefaultEngine();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Locale getDefaultLanguage() {
         return ttsInstance.getDefaultLanguage();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Set<String> getFeature(Locale locale) {
         return ttsInstance.getFeatures(locale);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Locale getLanguage() {
         return ttsInstance.getLanguage();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int isLanguageAvailable(Locale locale) {
         return ttsInstance.isLanguageAvailable(locale);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean isSpeaking() {
         return ttsInstance.isSpeaking();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int speak(String text, int queueMode, HashMap<String, String> params) {
         return ttsInstance.speak(text, queueMode, params);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int stop() {
         return ttsInstance.stop();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void shutdown() {
         ttsInstance.shutdown();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int setPitch(float pitch) {
         return ttsInstance.setPitch(pitch);
     }
 
-    @Override
-    public int playEarcon(String earcon, int queueMode, HashMap<String, String> params) {
-        return ttsInstance.playEarcon(earcon, queueMode, params);
-    }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int playSilence(long durationInMs, int queueMode, HashMap<String, String> params) {
         return ttsInstance.playSilence(durationInMs, queueMode, params);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean readyToUse() {
         return isReadyToUse;
