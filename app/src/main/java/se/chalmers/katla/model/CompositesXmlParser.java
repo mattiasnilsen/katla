01cacfcb849b2c5236808c17b5e159de2ca38965
Mattias Nilsen <kerp@live.se>
2014-10-12 11:38:20 +0200

    Added support for the input tag to the XML parser.

--- app/src/main/java/se/chalmers/katla/model/CompositesXmlParser.java
+++ app/src/main/java/se/chalmers/katla/model/CompositesXmlParser.java
@@ -4,6 +4,7 @@ import android.util.Xml;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -19,10 +20,12 @@ public class CompositesXmlParser {
 
     private final String COMPOSITE_TAG = "composite";
     private final String CATEGORY_TAG = "category";
+    private final String INPUT_TAG = "input";
 
     public List parse(InputStream in)  throws XmlPullParserException, IOException{
         try {
-            XmlPullParser parser = Xml.newPullParser();
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = factory.newPullParser();
             parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
             parser.setInput(in, null);
             parser.nextTag();
@@ -38,6 +41,7 @@ public class CompositesXmlParser {
 
         ICategory currentCategory = null;
         IComposite currentComposite = null;
+        Boolean insideInputTag = false;
         while (eventType != XmlPullParser.END_DOCUMENT) {
             if(eventType == XmlPullParser.START_TAG) {
                 if(parser.getName().equals(CATEGORY_TAG)) {
@@ -48,6 +52,8 @@ public class CompositesXmlParser {
                     }
                 } else if(parser.getName().equals(COMPOSITE_TAG)) {
                     currentComposite = new Composite();
+                } else if(parser.getName().equals(INPUT_TAG)) {
+                    insideInputTag = true;
                 }
             } else if(eventType == XmlPullParser.END_TAG) {
                 if(parser.getName().equals(COMPOSITE_TAG) && currentComposite != null) {
@@ -56,15 +62,30 @@ public class CompositesXmlParser {
                 } else if(parser.getName().equals(CATEGORY_TAG)) {
                     categories.add(currentCategory);
                     currentCategory = null;
+                } else if(parser.getName().equals(INPUT_TAG)) {
+                    insideInputTag = false;
                 }
             } else if(eventType == XmlPullParser.TEXT) {
                 if(currentComposite != null) {
                     currentComposite.setBaseText(parser.getText());
+                }
+                if(insideInputTag) {
+                    List<String> inputs = getInputTypes(parser.getText());
+                    currentComposite.setInputs(inputs);
                 }
             }
             eventType = parser.next();
         }
 
         return categories;
+    }
+
+    private List<String> getInputTypes(String input) {
+        List<String> inputs = new ArrayList<String>();
+        String[] inputArray = input.split(",");
+        for (int i = 0; i < inputArray.length; ++i) {
+            inputs.add(inputArray[i]);
+        }
+        return inputs;
     }
 }
