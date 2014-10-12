ea9f81f6f441f11609a1c4538503528644536ca2
Mattias Nilsen <kerp@live.se>
2014-10-12 11:25:28 +0200

    Moved javadoc comments to the IComposite interface.

--- app/src/main/java/se/chalmers/katla/model/Composite.java
+++ app/src/main/java/se/chalmers/katla/model/Composite.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Class that represents a composite in the model.
+ * The Composite class implements the IComposite interface and represents a composite object in the model.
  */
 public class Composite implements IComposite {
 
@@ -16,38 +16,21 @@ public class Composite implements IComposite {
         inputs = new ArrayList<String>();
     }
 
-    /**
-     * Returns the baseText that contains both the predefined baseText and any user input if there is any.
-     * @return the complete text
-     */
     @Override
     public String getText() {
         return baseText;
     }
 
-    /**
-     * Returns the baseText for this composite without any of the users input.
-     * @return the predefined text
-     */
     @Override
     public String getBaseText() {
         return baseText;
     }
 
-    /**
-     * Sets the base baseText for this composite.
-     * @param text the baseText to set
-
-     */
     @Override
     public void setBaseText(String text) {
         this.baseText = text;
     }
 
-    /**
-     * Sets the list of input activities that should be activated when this composite is clicked.
-     * @param inputs The list of the inputs to activate in order.
-     */
     @Override
     public void setInputs(List<String> inputs) {
         for(String string : inputs) {
@@ -55,10 +38,6 @@ public class Composite implements IComposite {
         }
     }
 
-    /**
-     * Returns the list of input activities that should be activated when this composite is clicked.
-     * @return The list the inputs to activate in order.
-     */
     @Override
     public List<String> getInputs() {
         return inputs;
