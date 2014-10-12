b8886b11888326955997aebc6c74cdf8ec6269c6
Mattias Nilsen <kerp@live.se>
2014-10-12 11:21:18 +0200

    Added support for different input types to model composites.

--- app/src/main/java/se/chalmers/katla/model/Composite.java
+++ app/src/main/java/se/chalmers/katla/model/Composite.java
@@ -1,11 +1,20 @@
 package se.chalmers.katla.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Class that represents a composite in the model.
  */
 public class Composite implements IComposite {
 
     private String baseText;
+    private List<String> inputs;
+
+    public Composite() {
+        baseText = new String();
+        inputs = new ArrayList<String>();
+    }
 
     /**
      * Returns the baseText that contains both the predefined baseText and any user input if there is any.
@@ -33,5 +42,25 @@ public class Composite implements IComposite {
     @Override
     public void setBaseText(String text) {
         this.baseText = text;
+    }
+
+    /**
+     * Sets the list of input activities that should be activated when this composite is clicked.
+     * @param inputs The list of the inputs to activate in order.
+     */
+    @Override
+    public void setInputs(List<String> inputs) {
+        for(String string : inputs) {
+            inputs.add(string);
+        }
+    }
+
+    /**
+     * Returns the list of input activities that should be activated when this composite is clicked.
+     * @return The list the inputs to activate in order.
+     */
+    @Override
+    public List<String> getInputs() {
+        return inputs;
     }
 }
--- app/src/main/java/se/chalmers/katla/model/IComposite.java
+++ app/src/main/java/se/chalmers/katla/model/IComposite.java
@@ -1,5 +1,7 @@
 package se.chalmers.katla.model;
 
+import java.util.List;
+
 /**
  * Created by Mattias on 2014-10-03.
  */
@@ -9,5 +11,7 @@ public interface IComposite {
     public String getBaseText();
     public void setBaseText(String text);
 
+    public void setInputs(List<String> inputs);
+    public List<String> getInputs();
 
 }
