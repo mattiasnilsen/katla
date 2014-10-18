52bda943c19f94ea458e808d028ded9f4fbf4545
Joel Gustafsson <kalioragus@gmail.com>
2014-10-18 14:57:37 +0200

    Made EventBus synchronized

--- app/src/main/java/se/chalmers/katla/eventBus/EventBus.java
+++ app/src/main/java/se/chalmers/katla/eventBus/EventBus.java
@@ -15,22 +15,22 @@ public class EventBus {
         this.eventListenerList = new ArrayList<EventListener>();
     }
 
-    public static EventBus getInstance() {
+    public static synchronized EventBus getInstance() {
         if (instance == null){
             instance = new EventBus();
         }
         return instance;
     }
 
-    public void registerListener(EventListener eventListener) {
+    public synchronized void registerListener(EventListener eventListener) {
         eventListenerList.add(eventListener);
     }
 
-    public void removeListener(EventListener eventListener) {
+    public synchronized void removeListener(EventListener eventListener) {
         eventListenerList.remove(eventListener);
     }
 
-    public void sendEvent(String s, Object o) {
+    public synchronized void sendEvent(String s, Object o) {
         for (EventListener ev : eventListenerList) {
             ev.receiveEvent(s, o);
         }
