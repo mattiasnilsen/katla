6be97d51b58a1d8b2ec83401280e82a044f962c5
Joel Gustafsson <kalioragus@gmail.com>
2014-10-27 20:43:57 +0100

    Added send message tests

---
+++ robolectricTests/src/test/java/se/chalmers/katla/test/modelTests/KatlaTests.java
@@ -0,0 +1,68 @@
+package se.chalmers.katla.test.modelTests;
+
+import android.telephony.SmsManager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.shadows.ShadowSmsManager;
+
+import se.chalmers.katla.model.Katla;
+import se.chalmers.katla.test.RobolectricKatlaTestRunner;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotSame;
+import static org.robolectric.Robolectric.shadowOf;
+
+/**
+ * Created by Joel on 27/10/2014.
+ */
+@RunWith(RobolectricKatlaTestRunner.class)
+public class KatlaTests {
+
+    @Test
+    public void sendMessageTest() {
+        Katla katla = Katla.getInstance();
+
+        ShadowSmsManager shadowSmsManager = shadowOf(SmsManager.getDefault());
+
+        String message = "Nu testar vi hur det funkar att skicka sms";
+        katla.setMessage(message);
+        katla.sendMessage();
+        assertEquals(message, shadowSmsManager.getLastSentTextMessageParams().getText());
+
+        message = "Går det att skicka konstiga tecken? åäö @$$£€€½§¬";
+        katla.setMessage(message);
+        katla.sendMessage();
+        assertEquals(message, shadowSmsManager.getLastSentTextMessageParams().getText());
+
+        // Test för tomt meddelande
+        message = "";
+        katla.setMessage(message);
+        katla.sendMessage();
+        assertNotSame(message, shadowSmsManager.getLastSentTextMessageParams().getText());
+
+        //Test för lågt meddelande
+        message = "Detta är ett meddelande som ska vara länge än gränsen för tecken vilken är 160." +
+                " Anstränga mig för att klara av att skriva ett så långt meddelande här, det blir" +
+                " inte lätt men det ska nog gå. Undrar hur många tecken vi har kvar till 160 nu" +
+                " kanske är bäst att kolla ojdå uppe i 262 redan.";
+        katla.setMessage(message);
+        katla.sendMessage();
+        //Misslyckas
+        //assertEquals(message, shadowSmsManager.getLastSentTextMessageParams().getText());
+
+        //Test för aslångt meddelande
+        message = "Detta är ett meddelande som ska vara länge än gränsen för tecken vilken är 160." +
+                "Anstränga mig för att klara av att skriva ett så långt meddelande här, det blir" +
+                "inte lätt men det ska nog gå. Undrar hur många tecken vi har kvar till 160 nu" +
+                "kanske är bäst att kolla ojdå uppe i 262 redan. Detta är ett meddelande som ska" +
+                " vara länge än gränsen för tecken vilken är 160." +
+                "Anstränga mig för att klara av att skriva ett så långt meddelande här, det blir" +
+                "inte lätt men det ska nog gå. Undrar hur många tecken vi har kvar till 160 nu" +
+                "kanske är bäst att kolla ojdå uppe i 262 redan.";
+        katla.setMessage(message);
+        katla.sendMessage();
+        // misslyckas
+        //assertEquals(message, shadowSmsManager.getLastSentTextMessageParams().getText());
+    }
+}
